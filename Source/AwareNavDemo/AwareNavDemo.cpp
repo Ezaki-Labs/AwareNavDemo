0ce4f82cb244937b7d818f24629b2e6a22305e10
Eduard Zakaryan <eduardzakaryan@mpg.io>
2025-08-03 23:36:57 +0100

    AwareNav Demo initital commit

---
+++ Source/AwareNavDemo/AwareNavDemo.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AwareNavDemo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AwareNavDemo, "AwareNavDemo" );
